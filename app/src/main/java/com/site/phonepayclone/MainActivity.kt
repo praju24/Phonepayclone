3a24ee14d88ecf987b472a47d0d22c875672f045
praju24 <prajaktanagrale11@gmail.com>
2021-07-06 11:24:38 +0530

    Initial commit

---
+++ app/src/main/java/com/site/phonepayclone/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.site.phonepayclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
